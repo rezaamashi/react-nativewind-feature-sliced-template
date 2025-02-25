540769e9e36b08c99b0b19c9c9f04576999ab302
Reza A'masyi <mnurrreza@gmail.com>
2025-02-25 17:53:34 +0700

    style(nativewind): Nativewind init

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@
 module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
-  extends: ['expo', '@feature-sliced'],
+  extends: ['expo', '@feature-sliced', 'plugin:tailwindcss/recommended'],
   ignorePatterns: ['/dist/*', '/*.config.js'],
   settings: {
     'import/resolver': {
@@ -17,6 +17,7 @@ module.exports = {
       {
         allow: [
           // Allow internal imports within the same segment
+          '**/app/{ui,styles,routes,analytics,api,config,lib,model}/*',
           '**/{pages,widgets,features,entities}/*/{ui,api,config,lib,model}/*',
           '**/shared/{ui,api,config,lib,model}/*',
         ],
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,3 +1,4 @@
+import 'app/styles/global.css';
 import { Home } from "pages";
 
 export default function App() {
